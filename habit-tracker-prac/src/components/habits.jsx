5d698cc82237745096e781d8a2136c152f018061
hyechan <dhgpcks@gmail.com>
2021-03-17 10:20:27 +0900

    pract success

--- habit-tracker-prac/src/components/habits.jsx
+++ habit-tracker-prac/src/components/habits.jsx
@@ -2,38 +2,20 @@ import React, { Component } from 'react';
 import Habit from './habit';
 
 class Habits extends Component {
-   state ={
-      habits:[
-         { id:'1', title: 'Reading', count: 0},
-         { id:'2', title: 'Wirting', count: 0},
-         { id:'3', title: 'Coding', count: 0},
-      ]
-   }
    HandlerIncrement=(habit)=>{
-      const habits = [...this.state.habits];
-      const no = habits.indexOf(habit);
-      habits[no].count++;
-      this.setState({habits});
+      this.props.onIncrement(habit);
    }
    HandlerDecrement=(habit)=>{
-      const habits = [...this.state.habits];
-      const no = habits.indexOf(habit);
-      const count = habits[no].count-1;
-      habits[no].count = count<0 ? 0 : count;
-      this.setState({habits});
+      this.props.onDecrement(habit);
    }
    HandlerDelete=(habit)=>{
-      const habits = this.state.habits.filter(item=>(
-         item.id !==habit.id
-      ))
-      console.log(habits);
-      this.setState({habits});
+      this.props.onDelete(habit);
    }
    render() {
       return (
          <div>
             {
-               this.state.habits.map((habit)=>(
+               this.props.habits.map((habit)=>(
                   <Habit
                   key={habit.id}
                   habit={habit}
